63c8b4c5ac716dd675dc6ca1039570e16bd6b197
El Barrent <el.barrent@gmail.com>
2023-09-18 16:22:17 +0300

    Add project files.

---
+++ Barrent.Common.WPF/Commands/RelayCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Barrent.Common.WPF.Commands;
+
+public class RelayCommand<T> : ICommand
+{
+    private readonly Predicate<T>? _canExecute;
+
+    private readonly Action<T> _execute;
+
+    public RelayCommand(Action<T> execute, Predicate<T>? canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(T parameter)
+    {
+        if (_canExecute == null)
+        {
+            return true;
+        }
+
+        return _canExecute(parameter);
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return CanExecute((T)parameter);
+    }
+
+    public void Execute(T parameter)
+    {
+        _execute(parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+        Execute((T)parameter);
+    }
+}
+
+public class RelayCommand : RelayCommand<object>
+{
+
+    public RelayCommand(Action<object> execute, Predicate<object>? canExecute = null) : base(execute, canExecute)
+    {
+    }
+}
---
+++ Barrent.Common.WPF/ViewModels/ViewModelBase.cs
@@ -0,0 +1,61 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Barrent.Common.WPF.ViewModels;
+
+public abstract class ViewModelBase : BindableBase, IDisposable
+{
+
+    private bool _isDisposed;
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Releases the memory associated to this class.
+    /// All derived classes must override this function to release
+    /// its respective managed and unmanaged resources.
+    /// </summary>
+    /// <param name="disposing">A value indicating whether the Dispose has been called by
+    /// this class or by the garbage collector finalizer.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_isDisposed) return;
+
+        if (disposing)
+        {
+            // Dispose only managed resources here.
+        }
+
+        // Dispose any unmanaged resources here.
+
+        _isDisposed = true;
+    }
+
+    protected bool SetValue<T>(ref T variable, T value, [CallerMemberName] string propertyName = null)
+    {
+        return SetProperty(ref variable, value, propertyName);
+    }
+
+    protected bool SetValue<T>(T currentValue, T newValue, Action<T> applyNewValue, [CallerMemberName] string propertyName = null)
+    {
+        return SetProperty(currentValue, newValue, applyNewValue, propertyName);
+    }
+
+    protected bool SetProperty<T>(T currentValue, T newValue, Action<T> applyNewValue, [CallerMemberName] string propertyName = null)
+    {
+        if (!EqualityComparer<T>.Default.Equals(currentValue, newValue))
+        {
+            applyNewValue(newValue);
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        return false;
+    }
+}
